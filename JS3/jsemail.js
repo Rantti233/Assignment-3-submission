ec3813b050c1fdcc01776a3f373b4f871610f6eb
Rantti233 <98371463+Rantti233@users.noreply.github.com>
2022-01-28 11:42:15 +0530

    Assignment3 submitted

---
+++ JS3/jsemail.js
@@ -0,0 +1,11 @@
+const email=prompt('Enter Email');
+if(email ==="stu@letsupgrade.com"){
+    alert('Hurray! you are logged in')
+}else{
+    alert('Try again with correct email');
+}
+
+const password=prompt('Enter pasword');
+password === "12345"
+?alert('Hurray! you are log in')
+: alert('Try again with correct passwod')
